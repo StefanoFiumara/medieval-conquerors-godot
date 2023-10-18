d7b5592468e9d8dab43504ed78a915b4a35aaeaa
Stefano Fiumara <sdf10517@gmail.com>
2023-10-18 13:55:24 -0500

    add card data and attributes

---
+++ MedievalConquerors.Godot/GameData/Cards/CardAttribute.cs
@@ -0,0 +1,8 @@
+﻿using Godot;
+
+namespace MedievalConquerors.GameData.Cards;
+
+public class CardAttribute : Resource
+{
+    // TODO: What goes here?
+}
---
+++ MedievalConquerors.Godot/GameData/Cards/CardData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace MedievalConquerors.GameData.Cards;
+
+public class CardData : Resource
+{
+    [Export] public string Title { get; set; }
+    
+    [Export] public string Description { get; set; }
+    
+    [Export] public List<CardAttribute> Attributes { get; set; }
+
+    [Export] public Sprite2D Image { get; set; }
+    
+    // TODO: CardType, Tags, Tooltip text
+}
