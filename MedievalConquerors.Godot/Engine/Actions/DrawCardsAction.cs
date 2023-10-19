9fba1103f9672a1fbd72fd11ee475e708b6196d1
Stefano Fiumara <sdf10517@gmail.com>
2023-10-19 00:08:19 -0500

    add DrawCardsAction

---
+++ MedievalConquerors.Godot/Engine/Actions/DrawCardsAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MedievalConquerors.Engine.Data;
+
+namespace MedievalConquerors.Engine.Actions;
+
+public class DrawCardsAction : GameAction
+{
+    public int Amount { get; }
+    public List<Card> DrawnCards { get; set; }
+
+    public DrawCardsAction(int amount)
+    {
+        Amount = amount;
+        DrawnCards = new List<Card>();
+    }
+}
