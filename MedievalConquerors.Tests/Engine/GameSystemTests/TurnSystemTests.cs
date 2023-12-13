815bd352b4dbf12534ecbc3ef69bff2aaff0bb52
Stefano Fiumara <sdf10517@gmail.com>
2023-12-13 15:43:56 -0600

    WIP turn system tests

---
+++ MedievalConquerors.Tests/Engine/GameSystemTests/TurnSystemTests.cs
@@ -0,0 +1,20 @@
+﻿using MedievalConquerors.Engine.GameComponents;
+using Xunit.Abstractions;
+
+namespace MedievalConquerors.Tests.Engine.GameSystemTests;
+
+public class TurnSystemTests : GameSystemTestFixture
+{
+    private readonly TurnSystem _underTest;
+
+    public TurnSystemTests(ITestOutputHelper output) : base(output)
+    {
+        _underTest = Game.GetComponent<TurnSystem>();
+    }
+
+    [Fact]
+    public void GameFactory_Creates_TurnSystem()
+    {
+        Assert.NotNull(_underTest);
+    }
+}
