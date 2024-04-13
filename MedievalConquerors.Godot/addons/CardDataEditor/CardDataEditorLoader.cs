e390e60d0f7eafd91d8220d708e79f0aeb60a42d
Stefano Fiumara <sdf10517@gmail.com>
2024-04-13 02:10:32 -0500

    refactor loader

--- MedievalConquerors.Godot/addons/CardDataEditor/CardDataEditorLoader.cs
+++ MedievalConquerors.Godot/addons/CardDataEditor/CardDataEditorLoader.cs
@@ -11,8 +11,8 @@ public partial class CardDataEditorLoader : EditorPlugin
 	private PackedScene _editorScene;
 	private PackedScene _libraryScene;
 	
-	private CardDataEditor _editorInstance;
-	private CardLibrary _libraryInstance;
+	private CardDataEditor _editor;
+	private CardLibrary _library;
 	
 	public override void _EnterTree()
 	{
@@ -24,64 +24,66 @@ public partial class CardDataEditorLoader : EditorPlugin
 		_editorScene = GD.Load<PackedScene>("res://addons/CardDataEditor/card_data_editor.tscn");
 		_libraryScene = GD.Load<PackedScene>("res://addons/CardDataEditor/Library/card_library.tscn");
 		
-		_editorInstance = _editorScene.Instantiate<CardDataEditor>();
+		_editor = _editorScene.Instantiate<CardDataEditor>();
+		_library = _libraryScene.Instantiate<CardLibrary>();
 		
-		var reloadButton = new Button { Text = "Reload Plugin" };
+		// TODO: Move the reload button somewhere else
+		var reloadButton = new Button { Text = "\u267b" };
+		reloadButton.TooltipText = "Reload Plugin";
 		reloadButton.Pressed += OnReloadPressed;
-		_editorInstance.GetNode("%editor_vbox_container").AddChild(reloadButton);
+		_editor.GetNode("%editor_vbox_container").AddChild(reloadButton);
 		
-		_libraryInstance = _libraryScene.Instantiate<CardLibrary>();
-		
+		//  TODO: Move the navigation header inside its own scene and add it to the container separately so all navigation logic can go here.
 		var container = new PanelContainer { Name = "Card Data Editor"};
 
-		container.AddChild(_editorInstance);
-		container.AddChild(_libraryInstance);
+		container.AddChild(_editor);
+		container.AddChild(_library);
 		
-		_libraryInstance.SearchResultClicked += LoadCard;
-		_libraryInstance.EditorNavigation += NavigateToEditor;
-		_editorInstance.LibraryNavigation += NavigateToLibrary;
+		_library.SearchResultClicked += LoadCard;
+		_library.EditorNavigation += NavigateToEditor;
+		_editor.LibraryNavigation += NavigateToLibrary;
 
-		_libraryInstance.Visible = false;
+		_library.Visible = false;
 		AddControlToDock(DockSlot.RightUl, container);
 	}
 
 	private void LoadCard(CardData card)
 	{
-		_editorInstance.LoadedData = card;
-		_libraryInstance.Visible = false;
-		_editorInstance.Visible = true;
+		_editor.LoadedData = card;
+		_library.Visible = false;
+		_editor.Visible = true;
 	}
 
 	private void NavigateToLibrary()
 	{
-		_libraryInstance.Visible = true;
-		_editorInstance.Visible = false;
+		_library.Visible = true;
+		_editor.Visible = false;
 	}
 	
 	private void NavigateToEditor()
 	{
-		_libraryInstance.Visible = false;
-		_editorInstance.Visible = true;
+		_library.Visible = false;
+		_editor.Visible = true;
 	}
 	
 	private void OnReloadPressed()
 	{
-		_libraryInstance.SearchResultClicked -= LoadCard;
-		_editorInstance?.QueueFree();
-		_libraryInstance?.QueueFree();
+		_library.SearchResultClicked -= LoadCard;
+		_editor?.QueueFree();
+		_library?.QueueFree();
 		
 		CallDeferred(nameof(CreateCardDataEditor));
 	}
 
 	public override void _ExitTree()
 	{
-		RemoveControlFromDocks(_editorInstance);
-		RemoveControlFromDocks(_libraryInstance);
-		_libraryInstance.SearchResultClicked -= LoadCard;
-		_libraryInstance.EditorNavigation -= NavigateToEditor;
-		_editorInstance.LibraryNavigation -= NavigateToLibrary;
-		_editorInstance.Free();
-		_libraryInstance.Free();
+		RemoveControlFromDocks(_editor);
+		RemoveControlFromDocks(_library);
+		_library.SearchResultClicked -= LoadCard;
+		_library.EditorNavigation -= NavigateToEditor;
+		_editor.LibraryNavigation -= NavigateToLibrary;
+		_editor.Free();
+		_library.Free();
 	}
 }
 #endif
