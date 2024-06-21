2bcfd41755a90182d7023d986a5bfdb8abaf6615
Stefano Fiumara <sdf10517@gmail.com>
2024-06-21 17:41:13 -0500

    add hit points attribute

---
+++ MedievalConquerors.Godot/Engine/Data/Attributes/HitPointsAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace MedievalConquerors.Engine.Data.Attributes;
+
+// TODO: System to use this attribute
+public class HitPointsAttribute : ICardAttribute
+{
+    public int Health { get; private set; }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+    }
+    
+    public void Reset()
+    {
+        
+    }
+
+    public ICardAttribute Clone()
+    {
+        return AttributeMapper.Clone(this);
+    }
+}
