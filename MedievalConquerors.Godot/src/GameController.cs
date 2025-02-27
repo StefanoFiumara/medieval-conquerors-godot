9badfd2978ac5f2d684f528683688990a1d806dd
Stefano Fiumara <sdfiumara@proton.me>
2025-02-26 23:51:50 -0600

    clean up game controller

--- MedievalConquerors.Godot/src/GameController.cs
+++ MedievalConquerors.Godot/src/GameController.cs
@@ -18,33 +18,32 @@ public partial class GameController : Node
 	
 	// IDEA: We can load different maps through this resource
 	[Export] private MapView _mapView;
-	
-	private Game _game;
+
 	private ILogger _log;
 	private HexMap _map;
 
-	public Game Game => _game;
+	public Game Game { get; private set; }
 
 	public override void _EnterTree()
 	{ 
 		_log = new GodotLogger(_logLevel);
 		_map = GameMapFactory.CreateHexMap(_mapView[MapLayerType.Terrain]);
-		_game = GameFactory.Create(_log, _map, _settings);
+		Game = GameFactory.Create(_log, _map, _settings);
 	}
 
 	public override void _Ready()
 	{
-		_game.Awake();
-		_game.Perform(new BeginGameAction(Match.LocalPlayerId));
+		Game.Awake();
+		Game.Perform(new BeginGameAction(Match.LocalPlayerId));
 	}
 
 	public override void _Process(double elapsed)
 	{
-		_game.Update();
+		Game.Update();
 	}
 
 	public override void _ExitTree()
 	{
-		_game.Destroy();
+		Game.Destroy();
 	}
 }
