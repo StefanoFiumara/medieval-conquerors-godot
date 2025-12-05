1e502d7928e44196951f3335904339fd42105699
Stefano Fiumara <sdfiumara@proton.me>
2025-12-05 14:22:47 -0600

    use Godot's RNG instead of C# Random

--- src/game/Extensions/CollectionExtensions.cs
+++ src/game/Extensions/CollectionExtensions.cs
@@ -24,14 +24,15 @@ public static class CollectionExtensions
     {
         // NOTE: Fisher Yates shuffle -> https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
         // TODO: Make rng globally available so that we can control the seed.
+        //
         // IDEA: Add a `RandomnessSystem` to control current seed and act as proxy for an RNG instance
-        var rng = new Random();
+        var rng = new RandomNumberGenerator();
 
         // TODO: There exists an rng.Shuffle method that does an in-place shuffle, is that a good candidate to replace this?
         for (var i = list.Count - 1; i > 0; --i)
         {
             // Select a random index
-            var r = rng.Next(0, i + 1);
+            var r = rng.RandiRange(0, i);
             // Swap
             (list[i], list[r]) = (list[r], list[i]);
         }
@@ -40,8 +41,8 @@ public static class CollectionExtensions
     public static T GetRandom<T>(this List<T> list)
     {
         // TODO: Make rng globally available so that we can control the seed.
-        var rng = new Random();
-        var randomIndex = rng.Next(0, list.Count);
+        var rng = new RandomNumberGenerator();
+        var randomIndex = rng.RandiRange(0, list.Count - 1);
         return list[randomIndex];
     }
 
