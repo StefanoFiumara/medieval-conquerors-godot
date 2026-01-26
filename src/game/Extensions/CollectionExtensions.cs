bdf2f9b842653bfbeac9149d6869cecfab15cf96
Stefano Fiumara <sdfiumara@proton.me>
2026-01-25 21:00:38 -0600

    notes

--- src/game/Extensions/CollectionExtensions.cs
+++ src/game/Extensions/CollectionExtensions.cs
@@ -19,15 +19,16 @@ public static class CollectionExtensions
         return card;
     }
 
+    // IDEA: Add a `RandomnessSystem` to control current seed and act as proxy for an RNG instance
+    // TODO: Upgrade to PCG32 randomness instead of relying on System.Random
+    //          - May need to import 3rd party library
+    //          - Could Godot's RandomNumberGenerator, which uses PCG32, but it does not play nice with unit tests that run outside of the engine.
+
     public static void Shuffle<T>(this List<T> list)
     {
         // NOTE: Fisher Yates shuffle -> https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-        // TODO: Make rng globally available so that we can control the seed.
-        //
-        // IDEA: Add a `RandomnessSystem` to control current seed and act as proxy for an RNG instance
         var rng = new Random();
 
-        // TODO: There exists an rng.Shuffle method that does an in-place shuffle, is that a good candidate to replace this?
         for (var i = list.Count - 1; i > 0; --i)
         {
             // Select a random index
@@ -39,7 +40,6 @@ public static class CollectionExtensions
 
     public static T GetRandom<T>(this List<T> list)
     {
-        // TODO: Make rng globally available so that we can control the seed.
         var rng = new Random();
         var randomIndex = rng.Next(0, list.Count - 1);
         return list[randomIndex];
